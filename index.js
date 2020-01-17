30568c14ea3e5e2fe5d70ffedb2ae7e7d6e42aff
César Ferreira de Jesus Junior <cjunior@unicad.local>
2020-01-17 09:27:56 -0300

    Define o middleware express.json()

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.json());
+
+app.delete('/users/:id', (req, res) => {
+    console.log(req.params);
+
     res.json({ message: 'Hello World' });
 });
 
