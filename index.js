d0b1f92f7bc1558a4dff1e2af2ded21d43a06b15
César <cesar.jnr@hotmail.com>
2020-01-18 15:59:37 -0300

    Instala a lib dotenv

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.use(express.json());
 
