731d45be8eca7aff2b1b28329dde6e643c2864bd
César <cesar.jnr@hotmail.com>
2020-01-19 15:33:50 -0300

    Realiza uma verificação para saber se o dev já existe antes de persisti-lo no banco de dados

--- src/app/controllers/DevController.js
+++ src/app/controllers/DevController.js
@@ -6,25 +6,29 @@ class DevController {
     async store(req, res) {
         const { github_username, techs, latitude, longitude } = req.body;
 
-        const githubApiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+        let dev = await new DevService().find({ github_username });
+        
+        if (!dev) {
+            const githubApiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
 
-        const { name = login, avatar_url, bio } = githubApiResponse.data;
-
-        const techsArray = techs.split(',').map(tech => tech.trim());
-
-        const location = {
-            type: 'Point',
-            coordinates: [longitude, latitude]
-        };
-
-        const dev = await new DevService().store({
-            github_username,
-            name,
-            avatar_url,
-            bio,
-            techs: techsArray,
-            location
-        });
+            const { name = login, avatar_url, bio } = githubApiResponse.data;
+    
+            const techsArray = techs.split(',').map(tech => tech.trim());
+    
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude]
+            };
+    
+            dev = await new DevService().store({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techsArray,
+                location
+            });
+        }
 
         return res.json(dev);
     }
--- src/app/services/DevService.js
+++ src/app/services/DevService.js
@@ -1,6 +1,10 @@
 const Dev = require('../models/Dev');
 
 class DevService {
+  async find(params) {
+    return await Dev.findOne(params);
+  }
+
   async store(data) {
     return await Dev.create(data);
   }
